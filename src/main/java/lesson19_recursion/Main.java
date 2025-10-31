dc02942d6417b420e66e1ba77b7c8b55a344737d
26Ninylichka <gmail.smoljarn@>
2025-10-31 12:22:47 +0100

    add lesson-19-recursion

---
+++ src/main/java/lesson19_recursion/Main.java
@@ -0,0 +1,15 @@
+package lesson19_recursion;
+
+public class Main {
+    static void main() {
+
+        int n = 2;
+        int result = MyMath.numberFibonachi(n);
+        System.out.println("Число фібоначі  є: " + n + " = " + result);
+
+
+    }
+
+
+}
+
